1b5bb381ea006a4ee2a51b9c9c3718f73ae85afc
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-02-26 19:53:43 -0500

    First to main

---
+++ ArrayTest.java
@@ -0,0 +1,15 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+
+        int[] arrayExample = new int[5];
+
+        arrayExample[0] = 1;
+        arrayExample[1] = 2;
+        arrayExample[2] = 3;
+        arrayExample[3] = 4;
+        arrayExample[4] = 5;
+
+        System.out.println(""); // Space
+        System.out.println("This Array Number is " + arrayExample[1]);
+    }
+}
