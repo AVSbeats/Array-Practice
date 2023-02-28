a40d9f25a10a3ed0d0eb3017f4d171d6433244be
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-02-28 15:37:25 -0500

    More Practice

--- ArrayTest.java
+++ ArrayTest.java
@@ -13,18 +13,26 @@ public class ArrayTest {
         System.out.println("The arrayExample for 1 is " + arrayExample[1]);
 
         System.out.println(""); // Space
-        int[] arrayExample2 = { 1, 2, 3, 4, 5 };
-        System.out.println("The arrayExample2 for 0 is " + arrayExample2[0]);
+        final int IncreaseByOne = 1;
+        for (int sub = 0; sub < 5; ++sub) {
+            arrayExample[sub] += IncreaseByOne;
+
+            System.out.println("After increase by one, arrayExample is " + arrayExample[sub]);
+        }
 
         System.out.println(""); // Space
-        final int Increase = 5;
-        arrayExample2[0] += Increase;
-        arrayExample2[1] += Increase;
-        arrayExample2[2] += Increase;
-        arrayExample2[3] += Increase;
-        arrayExample2[4] += Increase;
+        int[] arrayExample2 = { 1, 2, 3, 4, 5 };
+        System.out.println("The arrayExample2 for 4 is " + arrayExample2[4]);
 
-        System.out.println("After increase of 5, the arrayExample2 for 0 is " + arrayExample2[0]);
+        System.out.println(""); // Space
+        final int IncreaseByFive = 5;
+        arrayExample2[0] += IncreaseByFive;
+        arrayExample2[1] += IncreaseByFive;
+        arrayExample2[2] += IncreaseByFive;
+        arrayExample2[3] += IncreaseByFive;
+        arrayExample2[4] += IncreaseByFive;
+
+        System.out.println("After increase by 5, the arrayExample2 for 4 is " + arrayExample2[4]);
 
         System.out.println(""); // Space
     }
